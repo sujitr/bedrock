78645f2355a29758d3220ca530150d6b870043e1
Sujit Roy <sujitr@users.noreply.github.com>
2024-05-14 15:22:33 +0530

    Update lambda_function.py to use titan express FM instead of anthropic claude FM

--- lambda_function.py
+++ lambda_function.py
@@ -5,30 +5,29 @@ from datetime import datetime
 
 def generate_code_using_bedrock(message:str, language:str) -> str:
   
-  prompt_text = f'''
-  Human: Write {language} code for the following instructions: {message}.
-  Assistant: 
-  '''
+  prompt_text = f"""Write {language} code for {message}."""
   
   body = {
-    "prompt" : prompt_text,
-    "max_tokens_to_sample": 2048,
-    "temperature": 0.1,
-    "top_k": 250,
-    "top_p": 0.2,
-    "stop_sequence": ["\n\nHuman:"]
+    "inputText": prompt_text,
+    "textGenerationConfig" : {
+        "maxTokenCount": 2048,
+        "temperature": 0.1,
+        "topP": 0.2,
+        "stopSequences": ["User:"]
+    }
   }
   
   try:
-    bedrock = boto3.client("bedrock_runtime", region_name="us-east-1", config = botocore.config.Config(read_timeout=300, retries = {'max_attempts':3}))
-    response = bedrock.invoke_model(body=json.dumps(body), modelId = "anthropic.claude-v2")
+    bedrock = boto3.client("bedrock-runtime", region_name="us-east-1", config = botocore.config.Config(read_timeout=300, retries = {'max_attempts':3}))
+    response = bedrock.invoke_model(body=json.dumps(body), modelId = "amazon.titan-text-express-v1")
     response_content = response.get('body').read().decode('utf-8')
     resposne_data = json.loads(response_content)
-    code = resposne_data["completion"].strip()
+    code = resposne_data["results"][0]["outputText"]
+    print(code)
     return code
     
   except Exception as e:
-    print("Error generating the code")
+    print(f"Error generating the code : {e}")
     return ""
     
     
@@ -50,9 +49,9 @@ def lambda_handler(event, context):
   generated_code = generate_code_using_bedrock(message, langauge)
   
   if generated_code:
-    current_time = datetime.now().srftime('%H%M%S')
+    current_time = datetime.now().strftime('%H%M%S')
     s3_key = f'code-output/{current_time}.py'
-    s3_bucket = 'bedrock-bucket'
+    s3_bucket = 'bedrock-bucket-sk'
     
     save_code_to_s3_bucket(generated_code, s3_bucket, s3_key)
     
@@ -63,4 +62,3 @@ def lambda_handler(event, context):
     'statusCode':200,
     'body': json.dumps('Code generation complete')
   }
-  
